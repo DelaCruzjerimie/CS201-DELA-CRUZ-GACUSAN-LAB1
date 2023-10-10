252a0c9e6610a1953488d0b49b8948446fab69fa
jerimie dela cruz <147166414+DelaCruzjerimie@users.noreply.github.com>
2023-10-10 22:05:25 +0800

    feat: javascript code into my html

---
+++ script.js
@@ -0,0 +1,25 @@
+// 1. Create Variable name (firstFavNum) & store your favorite number.
+// 2. Create Variable name (secondFavNum) & store your second fav number.
+// 3. Add (firstFavNum & secondFavNumber).
+// 4. Subtract (firstFavNum & secondFavNumber).
+// 5. Multiply (firstFavNum & secondFavNumber).
+// 6. Divided (firstFavNum & secondFavNumber).
+// 7. Check (firstFavNum Mod secondFavNumber)
+// 8. Check the power of (firstFavNum)
+
+var firstFavNum = 8;
+var secondFavNum = 80;
+
+let sum = firstFavNum + secondFavNum;
+let difference = firstFavNum - secondFavNum;
+let product = firstFavNum * secondFavNum;
+let quotient = firstFavNum / secondFavNum;
+let remainder = firstFavNum % secondFavNum;
+let power = firstFavNum ** 4;
+
+console.log("The sum is " +sum);
+console.log("The difference is " +difference);
+console.log("The product is " +product);
+console.log("The quitient is " +quotient)
+console.log("The remainder is " +remainder);
+console.log("The power is " +power);
